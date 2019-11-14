8964bea29559de3bd8de710cd7a361388d4fa2be
David <dngames2006@gmail.com>
2019-11-13 20:02:37 -0600

    Created function template

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,11 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+def on_press(key):
+    pass
+
+def on_release(key):
+    pass
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    pass
